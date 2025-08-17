2abe620e147c741c92dec5b393135858855d9b89
Harsh007127 <harshsaxena9528@gmail.com>
2025-08-17 20:02:45 +0530

    Create task 2.py

---
+++ task 2.py
@@ -0,0 +1,33 @@
+# Assignment-2
+ 
+# Module 3: Control Structures in Python
+ 
+# Task 1: Check if a Number is Even or Odd
+
+ number =7
+ if number%2==0:
+     print('The number is evan.')
+ else:
+    print('The number is odd.')
+# Output:
+The number is odd 
+
+
+
+number =10
+ if number%2==0:
+     print('The number is even.')
+ else:
+    print('The number is odd.')
+# Output:
+The number is even.
+    
+# Task 2: Sum of Integers from 1 to 50 Using a Loop
+#sum of integers from 1to 50 using loop
+total=0
+for i in range(1,51):
+total+ = i
+print("sum from 1 to 51 is :",total)
+
+# Output
+sum from 1 to 51 is : 1275
